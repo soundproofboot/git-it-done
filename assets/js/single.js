00ad33ad35738be7c89f693a85eb215279895b18
C Bares <colinedwinbares@gmail.com>
2021-12-22 12:58:03 -0600

    pass repo name with query string, handle redirect

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,8 +1,26 @@
 var issueContainerEl = document.querySelector('#issues-container');
 var limitWarningEl = document.querySelector('#limit-warning');
+var repoNameEl = document.querySelector('#repo-name');
+
+var getRepoName = function() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split('=')[1];
+
+    if (repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+
+        getRepoIssues(repoName);
+    } else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace('./index.html');
+    }
+};
 
 var getRepoIssues = function(repo) {
     var apiUrl = 'https://api.github.com/repos/' + repo + '/issues?direction=asc';
+    // make a get request to url
     fetch(apiUrl).then(function(response) {
         // request was successful
         if (response.ok) {
@@ -16,7 +34,8 @@ var getRepoIssues = function(repo) {
                 }
             });
         } else {
-            alert('There was a problem with your request!');
+            // if not successful, redirect to homepage
+            document.location.replace('./index.html');
         }
     });
 };
@@ -69,4 +88,4 @@ var displayWarning = function(repo) {
     limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues('facebook/react');
+getRepoName()
